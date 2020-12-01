71ef7fe9b7e6ec8f5eaa0028b1c74fe1383c5c34
Rafael Bicudo <rafael.bicudo@uber.com>
2020-11-30 23:40:07 -0300

    group_share_per_category refactored and tested

--- src/plotly_utils.py
+++ src/plotly_utils.py
@@ -4,9 +4,10 @@ import pandas as pd
 def group_share_per_category(dataframe: pd.DataFrame, group_col: str, category_col: str):
     """
     Calculates the % share of a categorical variable in relation to a group (ie another categorical)
-    :dataframe: original dataframe in a subject
-    :param to_test_pd_series: pd series to be validated
-    :returns is_contained: boolen value regarding if the unique elements are contained in the list
+    :dataframe: original dataframe in a subject level
+    :param group_col: the name of the group column
+    :param category_col: the name of the category_col column
+    :returns group_share_per_category_df: df containing the % share each category has by group
     """
     # enable a dynamic percentual share column naming
     pct_share_col_name = f'share_per_{group_col}'
@@ -22,3 +23,20 @@ def group_share_per_category(dataframe: pd.DataFrame, group_col: str, category_c
         .assign(category_name = category_col)
     )
     return group_share_per_category_df
+
+def group_share_per_category_looper(
+        dataframe: pd.DataFrame, group_col: str, category_col_list: list
+    ):
+    """
+    Loops the group_share_per_category funct into a df based on a list of categories
+    :dataframe: original dataframe in a subject level
+    :param group_col: the name of the group column
+    :param category_col_list: tthe list of category columns
+    :returns agg_group_share_per_category_df: group_share_per_category concatened outputed dfs
+    """
+    agg_group_share_per_category_df = pd.DataFrame()
+    for category_col in category_col_list:
+        temp_df = group_share_per_category(dataframe, group_col, category_col)
+        agg_group_share_per_category_df = pd.concat([agg_group_share_per_category_df, temp_df])
+        del temp_df
+    return agg_group_share_per_category_df
