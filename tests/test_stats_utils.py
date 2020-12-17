a3f0fb5e91420a9ef2374d6711454e9cebfc915c
Rafael Bicudo <rafael.bicudo@uber.com>
2020-12-17 00:22:48 -0300

    initial (obvious) test for mean ttest tester

--- tests/test_stats_utils.py
+++ tests/test_stats_utils.py
@@ -2,6 +2,7 @@
 import sys
 import pytest
 import numpy as np
+from numpy.random import seed
 # Env tests
 try:
     sys.path.insert(1, './src')  # the type of path is string
@@ -10,11 +11,20 @@ except (ModuleNotFoundError, ImportError) as error_message:
     print("{} fileure".format(type(error_message)))
 
 @pytest.mark.parametrize(
-    "mu, sigma, sample_size", [
-        (0.0, 0.1, 50)
+    "add_mu, add_sigma, add_n, p_exp_result, power_exp_result", [
+        (0, 0, 0, False, False) # obvious non rejected case
     ]
 )
-def test_mean_ttest_analysis(_mu, sigma, sample_size):
-    """"""
-    # https://docs.scipy.org/doc//numpy-1.10.4/reference/generated/numpy.random.normal.html
-    pass
+
+def test_mean_ttest_analyzer(add_mu, add_sigma, add_n, p_exp_result, power_exp_result):
+    """Test different hypotehsis tests for mean comparisons"""
+    # random seed to generate the same ficitonal samples
+    seed(666)
+    population_1 = np.random.normal(0, 1, 50)
+    population_2 = np.random.normal(0 + add_mu, 1 + add_sigma, 50 + add_n)
+    p, power = su.mean_ttest_analyzer(population_1, population_2)
+     # reference p, power values
+    p_assessment = p < 0.1
+    power_assessment = power > 0.6
+    assert p_assessment
+    assert power_assessment
