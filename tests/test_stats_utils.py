1d1394b53e0803300fd9dac886b96f7b22192111
Rafael Bicudo <rafael.bicudo@uber.com>
2020-12-22 00:27:10 -0300

    Fixture and test for ols function done

--- tests/test_stats_utils.py
+++ tests/test_stats_utils.py
@@ -2,7 +2,9 @@
 import sys
 import pytest
 import numpy as np
+import pandas as pd
 from numpy.random import seed
+from sklearn.datasets import make_regression
 # Env tests
 try:
     sys.path.insert(1, './src')  # the type of path is string
@@ -30,3 +32,22 @@ def test_mean_ttest_analyzer(add_mu, add_sigma, add_n, p_exp_result, power_exp_r
     power_assessment = power > 0.6
     assert p_assessment == p_exp_result, f'the xp pvalue was {pvalue}'
     assert power_assessment == power_exp_result, f'the xp power was {power}'
+
+@pytest.fixture
+def _mock_ols_df():
+    """
+    Simulates a random df to be used at OLS in which the coef are know prior
+    """
+    features, target, mock_ols_coeffs = make_regression(
+        n_samples=100, n_features=10, n_informative=10, 
+        coef=True
+    )
+    features_df = pd.DataFrame(features, columns=[f'x_{i}' for i in range(10)])
+    target_df =pd.DataFrame(target, columns=['y'])
+    _mock_ols_df = pd.concat([features_df,target_df], axis=1)
+    return _mock_ols_df, mock_ols_coeffs
+
+def test_ols_dataframe_coeffs(_mock_ols_df, mock_ols_coeffs):
+    """Test OLS applier to pandas dataframes baseged on columns selection"""
+    coeffs = su.ols_dataframe_coeffs(_mock_ols_df)
+    assert coeffs == mock_ols_coeffs
